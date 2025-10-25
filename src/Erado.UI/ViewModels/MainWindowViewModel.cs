a1e1de171184714406c5d20ca758d0a763079ba8
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2025-10-25 23:11:18 +0200

    Initial Commit

---
+++ src/Erado.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Erado.UI.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/Erado.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Erado.UI.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
