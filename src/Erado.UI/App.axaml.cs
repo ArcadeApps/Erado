5dcba118c94f049db760281347f018c8855d629d
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2025-10-27 22:00:26 +0100

    setup generic .NET Host

--- src/Erado.UI/App.axaml.cs
+++ src/Erado.UI/App.axaml.cs
@@ -3,27 +3,52 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Erado.UI.ViewModels;
 using Erado.UI.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Erado.UI;
 
 public partial class App : Application
 {
+    public IHost? GlobalHost { get; private set; }
+    
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
+        var hostBuilder = CreateHostBuilder();
+        var host = hostBuilder.Build();
+        GlobalHost = host;
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = GlobalHost.Services.GetRequiredService<MainWindowViewModel>()
+            };
+            desktop.Exit += (sender, args) =>
+            {
+                GlobalHost.StopAsync(TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+                GlobalHost.Dispose();
+                GlobalHost = null;
             };
         }
-
+        
+        DataTemplates.Add(GlobalHost.Services.GetRequiredService<ViewLocator>());
+        
         base.OnFrameworkInitializationCompleted();
+        
+        await host.RunAsync();
     }
+    private static HostApplicationBuilder CreateHostBuilder()
+    {
+        var builder = Host.CreateApplicationBuilder(Environment.GetCommandLineArgs());
+        
+        builder.Services.AddTransient<ViewLocator>();
+        builder.Services.AddTransient<MainWindowViewModel>();
 
+        return builder;
+    }
 }
---
+++ src/Erado.UI/ViewLocator.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Erado.UI.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Erado.UI;
+public static class ViewLocatorHelpers
+{
+    public static IServiceCollection AddView<TViewModel, TView>(this IServiceCollection services)
+        where TView : Control, new()
+        where TViewModel : ViewModelBase
+    {
+        services.AddSingleton(new ViewLocator.ViewLocationDescriptor(typeof(TViewModel), () => new TView()));
+        return services;
+    }
+}
+
+public class ViewLocator : IDataTemplate
+{
+    private readonly Dictionary<Type, Func<Control>> _views = new();
+    
+    public ViewLocator(IEnumerable<ViewLocationDescriptor> descriptors)
+    {
+        _views = descriptors.ToDictionary(x => x.ViewModel, x => x.Factory);
+    }
+
+    public Control? Build(object? param) => _views[param!.GetType()]();
+
+    public bool Match(object? data) => data is not null && _views.ContainsKey(data.GetType());
+    
+    public record ViewLocationDescriptor(Type ViewModel, Func<Control> Factory);
+}
